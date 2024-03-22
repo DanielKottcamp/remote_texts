7e38a8163c61a12e0d337308132679a795a160b4
Daniel Kottcamp <dfk224@lehigh.edu>
2024-03-21 22:45:52 -0400

    initial complete version, compiles and sends texts, buttons not yet tested

--- main.cc
+++ main.cc
@@ -5,67 +5,83 @@
 #include <vector>
 #include <curl/curl.h>
 #include <wiringPi.h>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
 
 string apiKey;
-vector<string> phoneNums[9];
-vector<string> messages[9];
+string phoneNums[9];
+string textMess[9];
 string emergencyMessage;
 vector<string> emergencyNums;
 
-//GPIO pins, currently all temp and only 1 pin per function
-#define PhoneNum1 17 //assign GPIO pin 17 to first phone button
-#define MessageNum1 27 //GPIO pin 27 to first message button
-#define sendButton 22 //GPIO pin 22 to send message button
+// GPIO pins, currently all temp and only 1 pin per function
+#define PhoneNum1 17   // assign GPIO pin 17 to first phone button
+#define MessageNum1 27 // GPIO pin 27 to first message button
+#define sendButton 22  // GPIO pin 22 to send message button
 
 using namespace std;
 size_t writeCallback(void *contents, size_t size, size_t nmemb, string *userp);
-void sentText(const string &phoneNum, const string &message, const string &key);
-void sendText(const string &phoneNum, const string &message);
-void sendTextTest(const string &phoneNum, const string &message);
+void sentText(string phoneNum, string message, string key);
+void sendText(string phoneNum, string message);
+void sendTextTest(string phoneNum, string message);
 void readConfigFile();
+string trimSpaces(const string &str);
 
 int main(void)
 {
     string phoneNumber = "6095751848";
     string message = "Test message from the code rather than command line";
-    if (wiringPiSetupGpio() == -1) { //error out if issue with wiringpi library
-        std::cerr << "Failed to initialize WiringPi library" << std::endl;
-        return 1;
-    }
-    readConfigFile();
-    
-    //sendTextTest(phoneNumber, message);
+    // if (wiringPiSetupGpio() == -1)
+    // { // error out if issue with wiringpi library
+    //     std::cerr << "Failed to initialize WiringPi library" << std::endl;
+    //     return 1;
+    // }
+    //readConfigFile();
+
+    sendTextTest(phoneNumber, message);
 }
 
-
-void pinSetup(){
+void pinSetup()
+{
     pinMode(PhoneNum1, INPUT);
     pinMode(MessageNum1, INPUT);
     pinMode(sendButton, INPUT);
 }
 
-void buttonLoop(){ //break off main loop into it's own function to better facilitate debugging
-    bool NumsUsed[9] = {false}; 
+void buttonLoop()
+{ // break off main loop into it's own function to better facilitate debugging
+    bool NumsUsed[9] = {false};
     uint8_t message;
-    while(true){
+    while (true)
+    {
         int phoneNum1state = digitalRead(PhoneNum1);
         int message1state = digitalRead(MessageNum1);
         int sendstate = digitalRead(sendButton);
 
-        if(phoneNum1state == LOW){
+        if (phoneNum1state == LOW)
+        {
             NumsUsed[0] = !NumsUsed[0];
         }
-        if(message1state == LOW){
+        if (message1state == LOW)
+        {
             message = 1;
         }
-        if(sendstate == LOW){
-            for(int i = 0; i < 9; i++){
-                if(NumsUsed[i]){
-                    sendText(phoneNums[i], messages[message])
+        if (sendstate == LOW && message >= 0 && message < 10)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (NumsUsed[i])
+                {
+                    sendText(phoneNums[i], textMess[message]);
                 }
             }
-            message = NULL;
-            NumsUsed = {false};
+            message = 100;
+            for (int i = 0; i < 9; ++i)
+            {
+                NumsUsed[i] = false;
+            }
         }
     }
 }
@@ -76,7 +92,7 @@ size_t writeCallback(void *contents, size_t size, size_t nmemb, string *userp)
     return size * nmemb;
 }
 
-void sendText(const string &phoneNum, const string &message, const string &key)
+void sendText(string phoneNum, string message, string key)
 {
     CURL *curl;
     CURLcode res;
@@ -84,20 +100,26 @@ void sendText(const string &phoneNum, const string &message, const string &key)
     curl = curl_easy_init();
     if (curl)
     {
-        string postData = "phone=" + phoneNum + "&message=" + message;
-        string url = "https://textbelt.com/text";
-        string keyData = "key=" + key;
+        string encodedPhone = curl_easy_escape(curl, phoneNum.c_str(), phoneNum.length());
+        string encodedMessage = curl_easy_escape(curl, message.c_str(), message.length());
 
-        string args = /*url + " --data-urlencode " + */ postData + " -d " + keyData; // formats data for curl command
+        string postData = "phone=" + encodedPhone + "&message=" + encodedMessage + "&key=" + key;
+        string url = "https://textbelt.com/text";
+        //string keyData = "key=" + key;
+
+        //string args = /*url + " --data-urlencode " + */ postData + " -d " + keyData; // formats data for curl command
 
         // curl_easy_setopt(curl, CURLOPT_URL, fullCommand.c_str()); //sends curl command
 
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postFields.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData.c_str());
 
         string response;
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response); // retrieves response
+
+        cout << "Curl Sent:\n"
+                 << postData << endl;
 
         res = curl_easy_perform(curl); // performs operation
 
@@ -115,12 +137,12 @@ void sendText(const string &phoneNum, const string &message, const string &key)
     }
 }
 
-void sendText(const string &phoneNum, const string &message)
+void sendText(string phoneNum, string message)
 { // overload function, if key not included then use global key variable
     sendText(phoneNum, message, apiKey);
 }
 
-void sendTextTest(const string &phoneNum, const string &message)
+void sendTextTest(string phoneNum, string message)
 { // overload function uses free test key, limited use
     sendText(phoneNum, message, "textbelt");
 }
@@ -140,36 +162,47 @@ void readConfigFile()
         {
             continue; // if no =, something went wrong. skip this line
         }
-        string type = trimSpaces(line.substr(0, deliminator)); //get what value is being set in this line
-        line = line.substr(deliminator + 1); //remove from line
-        string val = trimSpaces(line); //get rest of line as value being set
-        if(type == "API"){
-            apiKey = &val;
+        string type = trimSpaces(line.substr(0, deliminator)); // get what value is being set in this line
+        line = line.substr(deliminator + 1);                   // remove from line
+        string val = trimSpaces(line);                         // get rest of line as value being set
+        if (type == "API")
+        {
+            apiKey = val;
         }
-        else if(type.substr(0, type.length()-2) == "PHONE"){ //phone numbers
-            int num = type.c_str()[type.length()-2] - '0'; //convert number after phone to integer
-            phoneNums[num] = &val; //add to vector
+        else if (type.substr(0, type.length() - 2) == "PHONE")
+        {                                                    // phone numbers
+            int num = type.c_str()[type.length() - 2] - '0'; // convert number after phone to integer
+            phoneNums[num] = val;                           // add to vector
         }
-        else if(type.substr(0, type.length()-2) == "MESSAGE"){ //messages numbers
-            int num = type.c_str()[type.length()-2] - '0'; //convert number after message to integer
-            messages[num] = &val; //add to vector
+        else if (type.substr(0, type.length() - 2) == "MESSAGE")
+        {                                                    // messages numbers
+            int num = type.c_str()[type.length() - 2] - '0'; // convert number after message to integer
+            textMess[num] = val;                            // add to vector
         }
-        else if(type.substr(0, type.length()-2) == "EMERGENCY"){
-            emergencyMessage = &val;
+        else if (type.substr(0, type.length() - 2) == "EMERGENCY")
+        {
+            emergencyMessage = val;
         }
-        else if(type.substr(0, type.length()-2) == "EMERGENCYSEND"){
+        else if (type.substr(0, type.length() - 2) == "EMERGENCYSEND")
+        {
             deliminator = line.find(",");
-            while(deliminator != line.npos){
-                string num = trimSpaces(line.substr(0, deliminator)); //issolate first num
-                emergencyNums.push_back(num); //push num into vector
-                line = line.substr(deliminator + 1); //remove from line
+            while (deliminator != line.npos)
+            {
+                string num = trimSpaces(line.substr(0, deliminator)); // issolate first num
+                emergencyNums.push_back(num);                         // push num into vector
+                line = line.substr(deliminator + 1);                  // remove from line
             }
         }
     }
     config.close();
 }
 
-string trimSpaces(string &str){
-    str.erase(remove_if(str.begin(), str.end(), isspace), str.end());
-    return str;
+string trimSpaces(const string &str) {
+    string trimmed;
+    for (char c : str) {
+        if (!isspace(c)) {
+            trimmed.push_back(c);
+        }
+    }
+    return trimmed;
 }
